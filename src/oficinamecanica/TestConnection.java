123d5ca65e8822543c7c39631572160fa5526659
Bruno Agnelo <brunoagnelo.almeida@gmail.com>
2024-11-06 07:33:46 -0400

    A criação das classes

--- src/oficinamecanica/TestConnection.java
+++ src/oficinamecanica/TestConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class TestConnection {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/oficina_mecanica"; // substitua pelo nome do seu banco de dados
-        String user = "root"; // substitua pelo seu usuário MySQL
-        String password = "minha_senha"; // substitua pela sua senha MySQL
+        String url = "jdbc:mysql://localhost:3306/oficina_mecanica";
+        String user = "root"; 
+        String password = "minha_senha";
 
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             System.out.println("Conexão com o banco de dados bem-sucedida!");
