4a055b59679de7148d4ff82e9c2d82a2957e8398
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-11-06 16:55:25 -0400

    Criação da classe ClienteDAO, ainda não implementada, fazer o teste de CRUD

---
+++ src/oficinamecanica/ClienteDAO.java
@@ -0,0 +1,88 @@
+package oficinamecanica;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+public class ClienteDAO {
+    
+    //Método para adicionar um novo cliete ao banco de daods
+    public void adicionarCliente(Cliente cliente) {
+        String sql = "INSERT INTO Cliente(nome, telefone, email) VALUES (?, ?, ?)";
+        
+        try (Connection conn = DatabaseConection.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, cliente.getNome());
+            stmt.setString(2, cliente.getTelefone());
+            stmt.setString(3, cliente.getEmail());
+            
+            stmt.executeUpdate();
+            System.out.println("Cliente adicionado com sucesso!");
+        } catch(SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao adicionar Cliente.");
+        }
+    }
+    
+    //Método para listar todos os clientes
+    public List<Cliente> listarClientes(){
+        List<Cliente> clientes = new ArrayList<>();
+        String sql = "SELECT * FROM Cliente";
+        
+        try(Connection conn = DatabaseConnection.getConnection(); PreparedStatement stmt = conn.preparedStatement(sql); ResultSet rs = stmt.executeQuery()){
+            while (rs.next()){
+                Cliente cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNome(rs.getString("nome"));
+                cliente.setTelefone(rs.getString("telefone"));
+                cliente.setEmail(rs.getString("email"));
+                clientes.add(cliente);
+            }
+            
+        } catch(SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao lista clientes.");
+        }
+        
+        return clientes;
+    }
+    
+    // Método para atualizar um cliente existente
+    public void atualizarCliente(Cliente cliente) {
+        String sql = "UPDATE Cliente SET nome = ?, telefone = ?, email = ? WHERE id = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, cliente.getNome());
+            stmt.setString(2, cliente.getTelefone());
+            stmt.setString(3, cliente.getEmail());
+            stmt.setInt(4, cliente.getId());
+
+            stmt.executeUpdate();
+            System.out.println("Cliente atualizado com sucesso!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao atualizar cliente.");
+        }
+    }
+
+    // Método para excluir um cliente
+    public void excluirCliente(int id) {
+        String sql = "DELETE FROM Cliente WHERE id = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+
+            stmt.executeUpdate();
+            System.out.println("Cliente excluído com sucesso!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao excluir cliente.");
+        }
+    }
+}
---
+++ src/oficinamecanica/OrdemServico.java
@@ -0,0 +1,82 @@
+package oficinamecanica;
+public class OrdemServico {
+    private int id;
+    private int clienteId;
+    private int veiculoId;
+    private int dataAbertura;
+    private int dataFechamento;
+    private float valorTotal;
+    private String status;
+    
+    //Construtores
+    public OrdemServico(){}
+
+    public OrdemServico(int id, int clienteId, int veiculoId, int dataAbertura, int dataFechamento, float valorTotal, String status) {
+        this.id = id;
+        this.clienteId = clienteId;
+        this.veiculoId = veiculoId;
+        this.dataAbertura = dataAbertura;
+        this.dataFechamento = dataFechamento;
+        this.valorTotal = valorTotal;
+        this.status = status;
+    }
+    
+    //Getters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(int clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public int getVeiculoId() {
+        return veiculoId;
+    }
+
+    public void setVeiculoId(int veiculoId) {
+        this.veiculoId = veiculoId;
+    }
+
+    public int getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(int dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public int getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(int dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public float getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
